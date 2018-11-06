7ebc4aa2bf63337543bb12cbe35eab33e80a38a5
Karol Przechodzeń <karolprzechodzen@gmail.com>
2018-11-06 21:13:40 +0100

    Make rzapka great again

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -14,29 +14,25 @@ public class Frog : MonoBehaviour
 
     [SerializeField]
     public int currentSadnessLevel = 10;
-    private Image image;
+    private SpriteRenderer spriteRenderer;
+    private RectTransform rectTransform;
     private float xSpeed;
     private float ySpeed;
-    
 
     void Start()
     {
-        image = gameObject.GetComponent<Image>();
-        image.sprite = sadSprite;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sadSprite;
+        rectTransform = gameObject.GetComponent<RectTransform>();
         initFlyDestination();
     }
 
     void Update()
     {
-    }
-
-    public void FlyAway()
-    {
-        Vector2 frogSize = gameObject.GetComponent<Image>().rectTransform.sizeDelta;
-        image.rectTransform.sizeDelta = new Vector2(frogSize.x - 4, frogSize.y - 4);
-        gameObject.transform.Translate(xSpeed, ySpeed, 0);
-        xSpeed = xSpeed < 0 ? xSpeed - 0.1f : xSpeed + 0.1f;
-        ySpeed = ySpeed < 0 ? ySpeed - 0.65f : ySpeed + 0.65f;
+        if (!IsSad())
+        {
+            FlyAway();
+        }
     }
 
     public void TakeFlowers(int flowers)
@@ -48,7 +44,7 @@ public class Frog : MonoBehaviour
     {
         Debug.Log("Frog received " + happiness + " happiness.");
         currentSadnessLevel -= happiness;
-        if(!IsSad())
+        if (!IsSad())
         {
             MakeHappy();
         }
@@ -61,14 +57,23 @@ public class Frog : MonoBehaviour
 
     private void MakeHappy()
     {
-        image.sprite = happySprite;
+        spriteRenderer.sprite = happySprite;
+    }
+
+    private void FlyAway()
+    {
+        Vector2 frogSize = rectTransform.sizeDelta;
+        rectTransform.sizeDelta = new Vector2(frogSize.x - 4, frogSize.y - 4);
+        gameObject.transform.Translate(xSpeed, ySpeed, 0);
+        xSpeed = xSpeed < 0 ? xSpeed - 0.1f : xSpeed + 0.1f;
+        ySpeed = ySpeed < 0 ? ySpeed - 0.65f : ySpeed + 0.65f;
     }
 
     private void initFlyDestination()
     {
         var rnd = UnityEngine.Random.value;
         var rnd1 = UnityEngine.Random.value;
-     
+
         xSpeed = rnd > 0.5f ? 5f : -5f;
         ySpeed = rnd1 > 0.5f ? 5f : -5f;
     }
--- Assets/Scripts/OnSceneClick.cs
+++ Assets/Scripts/OnSceneClick.cs
@@ -4,73 +4,72 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class OnSceneClick : MonoBehaviour {
+public class OnSceneClick : MonoBehaviour
+{
 
-	private GameObject flowerPrefab;
-	private List<GameObject> spawnedFlowers;
-	private GameObject frogPrefab;
-	private Frog frogScript;
-	private List<GameObject> happyFrogs;
-	private GameObject currentFrog;
+    private GameObject flowerPrefab;
+    private List<GameObject> spawnedFlowers;
+    private GameObject frogPrefab;
+    private Frog frogScript;
+    private List<GameObject> happyFrogs;
+    private GameObject currentFrog;
 
-	void Start () {
-		this.frogScript = GameObject.Find("Frog").GetComponent<Frog>();
-		this.flowerPrefab = Resources.Load<GameObject>("Prefabs/Flower");
-		this.frogPrefab = Resources.Load<GameObject>("Prefabs/Frog");
-		this.spawnedFlowers = new List<GameObject>();
-		this.happyFrogs = new List<GameObject>();
-		this.currentFrog = GameObject.Find("Frog");
-	}
+    void Start()
+    {
+        this.frogScript = GameObject.Find("Frog").GetComponent<Frog>();
+        this.flowerPrefab = Resources.Load<GameObject>("Prefabs/Flower");
+        this.frogPrefab = Resources.Load<GameObject>("Prefabs/Frog");
+        this.spawnedFlowers = new List<GameObject>();
+        this.happyFrogs = new List<GameObject>();
+        this.currentFrog = GameObject.Find("Frog");
+    }
 
     void OnGUI()
     {
-		if(Event.current.type == EventType.MouseDown)
+        if (Event.current.type == EventType.MouseDown)
         {
             spawnFlower();
         }
     }
 
-	void Update () {
-		this.spawnedFlowers.ForEach(flower => {
-			flower.transform.Rotate(new Vector3(0, 0, 2));
-			flower.transform.position = Vector3.MoveTowards(flower.transform.position, this.frogScript.transform.position, 15);
+    void Update()
+    {
+        this.spawnedFlowers.ForEach(flower =>
+        {
+            flower.transform.Rotate(new Vector3(0, 0, 2));
+            flower.transform.position = Vector3.MoveTowards(flower.transform.position, this.frogScript.transform.position, 15);
 
-			if(flower.transform.position == frogScript.transform.position) 
-			{
-				this.spawnedFlowers.Remove(flower);
-				Destroy(flower, 0);
-				this.frogScript.TakeFlowers(10);
-			}
-		});
+            if (flower.transform.position == frogScript.transform.position)
+            {
+                this.spawnedFlowers.Remove(flower);
+                Destroy(flower, 0);
+                this.frogScript.TakeFlowers(10);
+            }
+        });
 
-		foreach (var frogo in happyFrogs)
-		{
-			frogo.GetComponent<Frog>().FlyAway();
-		}
-
-		if(!frogScript.IsSad())
-		{
-			var frogo = Instantiate(this.frogPrefab, this.frogScript.transform.position, Quaternion.identity);
-			frogo.transform.SetParent(GameObject.Find("Canvas").transform);
-			this.happyFrogs.Add(this.currentFrog);
-			StartCoroutine(Remover(this.currentFrog));
-			this.currentFrog = frogo;
-			this.frogScript = this.currentFrog.GetComponent<Frog>();
-		}
-	}
+        if (!frogScript.IsSad())
+        {
+            var frogo = Instantiate(this.frogPrefab, this.frogScript.transform.position, Quaternion.identity);
+            frogo.transform.SetParent(GameObject.Find("Canvas").transform);
+            this.happyFrogs.Add(this.currentFrog);
+            StartCoroutine(Remover(this.currentFrog));
+            this.currentFrog = frogo;
+            this.frogScript = this.currentFrog.GetComponent<Frog>();
+        }
+    }
 
     IEnumerator Remover(GameObject frogo)
     {
         yield return new WaitForSeconds(2);
-		happyFrogs.Remove(frogo);
-		Destroy(frogo);
+        happyFrogs.Remove(frogo);
+        Destroy(frogo);
     }
 
-	void spawnFlower()
-	{
-		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		GameObject spawnedFlower = Instantiate(this.flowerPrefab, mousePosition, Quaternion.identity);
-		spawnedFlower.transform.SetParent(GameObject.Find("Canvas").transform);
-		this.spawnedFlowers.Add(spawnedFlower);
-	}
+    void spawnFlower()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GameObject spawnedFlower = Instantiate(this.flowerPrefab, mousePosition, Quaternion.identity);
+        spawnedFlower.transform.SetParent(GameObject.Find("Canvas").transform);
+        this.spawnedFlowers.Add(spawnedFlower);
+    }
 }
