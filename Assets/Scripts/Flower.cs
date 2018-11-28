a90b7197e698f67ff5de32431c0943794feadbf8
sw30 <stanislaw.wolny30@gmail.com>
2018-11-28 19:09:16 +0100

    150MB SIEMNAKO

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Flower {
     public GameObject gameObject {get; set; }
+	private int rotateRadius;
 
 	public Flower(GameObject flower) 
 	{
 		this.gameObject = flower;
+		this.rotateRadius = UnityEngine.Random.value > .5f ? 10 : -10;
 	}
 
 	public void FlyToFrog(GameObject frogObject)
 	{
-		gameObject.transform.Rotate(new Vector3(0, 0, 2));
+		gameObject.transform.Rotate(new Vector3(0, 0, rotateRadius));
 		gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, frogObject.transform.position, 15);
 	}
 
