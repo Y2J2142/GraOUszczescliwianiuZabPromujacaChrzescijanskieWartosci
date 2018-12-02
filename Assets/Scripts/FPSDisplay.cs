5fb82f3dff08e93303508d6286077fd4b7db1074
Karol Przechodzeń <karolprzechodzen@gmail.com>
2018-12-02 10:31:54 +0100

    Wylaczenie fpsow poza gra bo zaslanialy

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -19,6 +19,10 @@ public class FPSDisplay : MonoBehaviour
 
     void OnGUI()
     {
+        if (!MenuController.isGameActive())
+        {
+            return;
+        }
         if (!showFPS)
         {
             return;
